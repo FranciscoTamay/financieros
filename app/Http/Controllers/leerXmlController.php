53a0f2ca395de558bea4de42c6220a3c8c329449
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2023-06-18 20:12:56 -0600

    leer XMl

--- app/Http/Controllers/leerXmlController.php
+++ app/Http/Controllers/leerXmlController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class leerXmlController extends Controller
@@ -17,8 +18,32 @@ class leerXmlController extends Controller
         // Convierte el contenido XML en un objeto SimpleXMLElement
         $xml = new \SimpleXMLElement($contenidoXML);
 
-        // Accedemos a los campos a necesitar
-        $rfc = $xml->Rfc;
+        // Accedemos a los campos a necesitar del xml para despues guardarlos en una tabla 
+        foreach ($xml->datos as $datos) {
+            // Convertir la fecha al formato "DD/MM/AA"
+            $fecha = $datos->Fecha;
+            $fechaFormateada = date('d/m/y', strtotime($fecha));
+            // rfc del emisor 
+            $emisor = $datos->Receptor;
+            $rfcEmisor = $emisor->Rfc;
+            // nombre de la empresa
+            $nombreEmpresa = $emisor->Nombre;
+
+            $concepto = $datos->Concepto;
+            $producto = $concepto->Descripcion;
+            $importe = $concepto->Importe;
+
+            // Guardar en la base de datos
+            DB::table('concentrado')->insert([
+                'fecha'=>$fechaFormateada,
+                'rfc'=>$rfcEmisor,
+                'razon_social'=>$nombreEmpresa,
+                'producto'=>$producto,
+                'importe'=>$importe
+            ]);
+
+            
+        }
     }
 
 
